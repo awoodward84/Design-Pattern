30b18d7c9453443c76669f9ea2c3e2992e2e1bca
Michael Woodward <m.woodward1017@gmail.com>
2021-04-09 00:37:35 -0700

    Added java doc comment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-
+    /**
+     * Michael Woodward
+     * SER316 Spring 21 Session B
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello new World!");
     }
