bb0db93eae2b6a677495ebe831b159e09e8501e8
Michael Woodward <m.woodward1017@gmail.com>
2021-04-14 21:32:18 -0700

    Added sparky operation for strategy pattern

---
+++ src/main/java/SparkyAttackOperation.java
@@ -0,0 +1,26 @@
+public class SparkyAttackOperation implements CodeAmonStrategyBase{
+
+    @Override
+    public Attack codeAMonAttack(CodeAmon attacker,int num) {
+        String desc = "";
+        Attack attack = null;
+        if (num == 0) {
+            desc = " uses fire breath, increasing attack stat by 10%";
+            attacker.stats.attack *= 1.10;
+            attack = new Attack(0, "None");
+        } else if (num == 1) {
+            desc = " uses Inferno";
+            attack = new Attack(attacker.stats.attack, "Fire");
+        } else if (num == 2) {
+            desc = " uses spiked tail ";
+            attack = new Attack(attacker.stats.attack, "Normal");
+            //System.out.println("Attack value: " + attacker.stats.attack);
+        } else {
+            desc = " uses Lightning spell";
+            attack = new Attack(attacker.stats.attack, "Ground");
+        }
+        System.out.println(attacker.name.toString().toLowerCase() + desc);
+        return attack;
+        }
+
+    }
