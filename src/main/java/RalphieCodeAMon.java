be508a92b4add094df4f4194a31e2087a46a2c92
Michael Woodward <m.woodward1017@gmail.com>
2021-04-16 00:19:08 -0700

    Created to implement factory pattern

---
+++ src/main/java/RalphieCodeAMon.java
@@ -0,0 +1,11 @@
+
+public class RalphieCodeAMon extends CodeAmon{
+
+    public RalphieCodeAMon() {
+        setName(Name.RALPHIE);
+        setType("Ground");
+        getStats();
+        getDescription();
+        getStrategy();
+    }
+}
