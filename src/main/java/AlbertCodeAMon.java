6bdf52724bdef47c6b598747f1b28779dd766081
Michael Woodward <m.woodward1017@gmail.com>
2021-04-16 00:17:46 -0700

    Creatd factory pattern code a mon

---
+++ src/main/java/AlbertCodeAMon.java
@@ -0,0 +1,13 @@
+
+
+public class AlbertCodeAMon extends CodeAmon{
+
+
+    public AlbertCodeAMon() {
+        setName(Name.ALBERT);
+        setType("Water");
+        getStats();
+        getDescription();
+        getStrategy();
+    }
+}
