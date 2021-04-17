3fc1feb6afa521643437d5a4b30a5dd204a52648
Michael Woodward <m.woodward1017@gmail.com>
2021-04-17 00:08:08 -0700

    Added javadoc comments

--- src/main/java/AlbertCodeAMon.java
+++ src/main/java/AlbertCodeAMon.java
@@ -1,7 +1,9 @@
 
 
 public class AlbertCodeAMon extends CodeAmon{
-
+    /**
+     * Constructor implementing a specific type of code a mon.
+     */
 
     public AlbertCodeAMon() {
         setName(Name.ALBERT);
