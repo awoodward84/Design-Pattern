73f47cacbf34137394211115b7b7c26bc7cd8547
Michael Woodward <m.woodward1017@gmail.com>
2021-04-17 00:08:28 -0700

    Added javadoc comments

--- src/main/java/BullyCodeAMon.java
+++ src/main/java/BullyCodeAMon.java
@@ -3,7 +3,9 @@ public class BullyCodeAMon extends CodeAmon {
 
 
     public BullyCodeAMon() {
-
+    /**
+    * Constructor implementing a specific type of code a mon.
+    */
         setName(Name.BULLY);
         setType("Normal");
         getStats();
